1dd0f777bb6713f5c843c50f8145826858e23351
Cameron McDonald <2570726m@student.gla.ac.uk>
2023-12-29 17:15:42 +0000

    Remade directory path. Object detection now runs until keyboard interupt from user. Code now only announces vehicle objects and there position.

--- src/object_detection.py
+++ src/object_detection.py
@@ -62,36 +62,25 @@ def main():
     obj_runtime_param = sl.ObjectDetectionRuntimeParameters()
     obj_runtime_param.detection_confidence_threshold = 80
 
-    iter = 0
-    while iter < 100:
-        zed.grab()
-        zed.retrieve_objects(objects, obj_runtime_param)
-        if objects.is_new :
-            obj_array = objects.object_list
-            print(str(len(obj_array))+" Object(s) detected\n")
-            if len(obj_array) > 0 :
-                first_object = obj_array[0]
-                print("First object attributes:")
-                print(" Label '"+repr(first_object.label)+"' (conf. "+str(int(first_object.confidence))+"/100)")
-                if obj_param.enable_tracking :
-                    print(" Tracking ID: "+str(int(first_object.id))+" tracking state: "+repr(first_object.tracking_state)+" / "+repr(first_object.action_state))
-                position = first_object.position
-                velocity = first_object.velocity
-                dimensions = first_object.dimensions
-                print(" 3D position: [{0},{1},{2}]\n Velocity: [{3},{4},{5}]\n 3D dimentions: [{6},{7},{8}]".format(position[0],position[1],position[2],velocity[0],velocity[1],velocity[2],dimensions[0],dimensions[1],dimensions[2]))
-                if first_object.mask.is_init():
-                    print(" 2D mask available")
-
-                print(" Bounding Box 2D ")
-                bounding_box_2d = first_object.bounding_box_2d
-                for it in bounding_box_2d :
-                    print("    "+str(it),end='')
-                print("\n Bounding Box 3D ")
-                bounding_box = first_object.bounding_box
-                for it in bounding_box :
-                    print("    "+str(it),end='')
-
-        iter = iter +1
+    try:
+        while True:
+            zed.grab()
+            zed.retrieve_objects(objects, obj_runtime_param)
+            if objects.is_new :
+                obj_array = objects.object_list
+                if len(obj_array) > 0 :
+                    first_object = obj_array[0]
+                    if repr(first_object.label) == "Vehicle":
+                        print("First object attributes:")
+                        print(" Label '"+repr(first_object.label)+"' (Confidence Level. "+str(int(first_object.confidence))+"/100)")
+                        if obj_param.enable_tracking :
+                            print(" Tracking ID: "+str(int(first_object.id))+" tracking state: "+repr(first_object.tracking_state)+" / "+repr(first_object.action_state))
+                        position = first_object.position
+                        velocity = first_object.velocity
+                        dimensions = first_object.dimensions
+                        print(" 3D position: [{0},{1},{2}]\n Velocity: [{3},{4},{5}]\n 3D dimentions: [{6},{7},{8}]".format(position[0],position[1],position[2],velocity[0],velocity[1],velocity[2],dimensions[0],dimensions[1],dimensions[2]))
+    except KeyboardInterrupt:
+        pass
 
     # Close the camera
     zed.disable_object_detection()
